802e3895d0dbae5fc079b56834197e3070c007f9
Samuel C. Tyler <skamansam@gmail.com>
2023-05-24 10:21:39 -0400

    create skeleton for sveltekit app

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script>
+  import "../app.css";
+
+  import { setContext } from 'svelte';
+  import { writable } from 'svelte/store';
+  import { player } from '../lib/player';
+
+  /** @type {import('./$types').LayoutData} */
+  // export let data;
+
+  // Create a store and update it when necessary...
+  const user = writable();
+  $: user.set(player);
+
+  // ...and add it to the context for child components to access
+  setContext('user', user);
+</script>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
+
+<svelte:head> <title>Energy Generator</title> </svelte:head>
+
+<slot />
